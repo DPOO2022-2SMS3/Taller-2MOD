f42ec42c4797c757e4021d1cdf87f62bece8876b
sebastian ramirez <sebastramireza@gmail.com>
2022-09-05 22:27:15 -0500

    Entrega Final - Taller 2 Modificado

--- Taller 2/src/Modelo/Restaurante.java
+++ Taller 2/src/Modelo/Restaurante.java
@@ -5,9 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
